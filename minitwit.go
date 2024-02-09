c0a2e644d2d7862f46f13da60c796184fb5375bf
volcanodoryshrimpnettuba <111703011+volcanodoryshrimpnettuba@users.noreply.github.com>
2024-02-09 13:35:09 +0100

    refactored test should work for GO

--- minitwit.go
+++ minitwit.go
@@ -62,6 +62,13 @@ func query_DB(query string) {
 
 }
 
+// def query_db(query, args=(), one=False):
+//     """Queries the database and returns a list of dictionaries."""
+//     cur = g.db.execute(query, args)
+//     rv = [dict((cur.description[idx][0], value)
+//                for idx, value in enumerate(row)) for row in cur.fetchall()]
+//     return (rv[0] if rv else None) if one else rv
+
 func main() {
 	r := mux.NewRouter()
 	// Define routes
